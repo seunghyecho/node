f241236abfaa95545b8ebd77087c34d617b3a461
seunghyecho <tmdgp523@naver.com>
2024-12-29 15:07:13 +0900

    로그아웃, 리다이렉트 버그 수정

--- lecture/controllers/auth.js
+++ lecture/controllers/auth.js
@@ -47,7 +47,7 @@ exports.login = (req, res, next) => {
 // app.use(passport.authenticate("kakao"));
 // app.use((req, res, next) => passport.authenticate("kakao")(req, res, next));
 
-exports.logout = () => {
+exports.logout = (req, res, next) => {
   // 세션쿠키 {123123123 : 1} 를  { } 로 없애버림. 브라우저 connect.sid가 남아있어도
   req.logout(() => {
     res.redirect("/");
