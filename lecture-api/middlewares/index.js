10adf0b1fa20eff6de9163911875cc29f918e056
geosoft-shcho <tmdgp523@gmail.com>
2025-01-20 14:26:16 +0900

    서버 생성(v1)

--- lecture-api/middlewares/index.js
+++ lecture-api/middlewares/index.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     // passport 통해서 로그인 함
@@ -6,11 +8,34 @@ exports.isLoggedIn = (req, res, next) => {
     res.status(403).send("로그인 필요");
   }
 };
+
 exports.isNotLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     next();
   } else {
     const message = encodeURIComponent("로그인한 상태");
     res.redirect(`/?error=${message}`); // localhost:8001?error=메세지
+  }
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    res.locals.decoded = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    return next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      res.statue(419).json({
+        code: 419,
+        message: "토큰이 만료되었습니다.",
+      });
+    }
+
+    return res.statue(401).json({
+      code: 401,
+      message: "유효하지 않은 토큰입니다.",
+    });
   }
 };
---
+++ lecture-api/routes/v1.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { verifyToken } = require("../middlewares");
+const { createToken, tokenTest } = require("../controllers");
+
+const router = express.Router();
+
+// /v1/token
+router.post("/token", createToken); // req.body.clientSecret
+router.get("/test", verifyToken, tokenTest);
+
+module.exports = router;
