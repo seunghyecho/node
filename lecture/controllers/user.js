eb04a117a5a61149db976f2e0bd76638c3579a31
seunghyecho <tmdgp523@naver.com>
2025-01-30 17:46:06 +0900

    jest 사용해 단위 테스트

--- lecture/controllers/user.js
+++ lecture/controllers/user.js
@@ -1,20 +1,29 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
+const { follow } = require("../services/user");
 
 // follow
 exports.follow = async (req, res, next) => {
   // req.user.id(내 아이디), req.params.id( 팔로잉 하려는 사람의 id)
   try {
-    const user = await User.findOne({
-      where: { id: req.user.id }, // 나를 먼저 찾기
-    });
-    if (user) {
-      // db 에 데이터가 없을 경우
-      await user.addFollowing(parseInt(req.params.id, 10));
+    const result = await follow(req.user.id, req.params.id);
+    if (result === "ok") {
       res.send("success");
-    } else {
+    } else if (result === "no user") {
       res.status(404).send("no user");
     }
+
+    // services/user 로 분리
+    // const user = await User.findOne({
+    //   where: { id: req.user.id }, // 나를 먼저 찾기
+    // });
+    // if (user) {
+    //   // db 에 데이터가 없을 경우
+    //   await user.addFollowing(parseInt(req.params.id, 10));
+    //   res.send("success");
+    // } else {
+    //   res.status(404).send("no user");
+    // }
   } catch (error) {
     console.error(error);
     next(error);
